a4a5121a1e925d2c6ad41d68f5da1cdfbe411fcc
KrlosPK <centille095@gmail.com>
2024-07-26 12:52:53 -0500

    feat(main): add cors middleware

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app.database.config import engine
 from app.api.models.tables import Base
 from starlette.responses import RedirectResponse
 from app.api.routers.endpoints import router
+from starlette.middleware.cors import CORSMiddleware
 
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
@@ -14,5 +15,13 @@ app = FastAPI()
 def main():
     return RedirectResponse("/docs")
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
