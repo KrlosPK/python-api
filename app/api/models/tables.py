9fb760c727904794d036ae446c9bca5b2eddc70f
KrlosPK <centille095@gmail.com>
2024-07-26 11:59:24 -0500

    fix: add table string limits

--- app/api/models/tables.py
+++ app/api/models/tables.py
@@ -8,15 +8,15 @@ Base = declarative_base()
 class Aliado(Base):
     __tablename__ = "aliado"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    nombre = Column(String)
-    ubicacion = Column(String)
+    nombre = Column(String(255))
+    ubicacion = Column(String(255))
     aporteMonetario = Column(Float)
 
 
 class Dragon(Base):
     __tablename__ = "dragon"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    nombre = Column(String)
+    nombre = Column(String(255))
     edad = Column(Integer)
     altura = Column(Float)
     numeroVictorias = Column(Integer)
@@ -25,6 +25,6 @@ class Dragon(Base):
 class Jinete(Base):
     __tablename__ = "jinete"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    nombre = Column(String)
-    edad = Column(String)
+    nombre = Column(String(255))
+    edad = Column(String(255))
     fechaMontura = Column(Date)
